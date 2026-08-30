9b61d777cf22f6b5710d5c874803d84bc94893e9
Iziken <ascetic.iziken@gmail.com>
2026-08-30 23:56:28 +0700

    hw10: add POM structure in project

---
+++ tests/pages/slots-page.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from "@playwright/test";
+import { ROUTES } from "../helpers/user";
+
+export class SlotsPage {
+  page: Page;
+  dateInput: Locator;
+  timeInput: Locator;
+  addSubmitButton: Locator;
+  firstSlotCard: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.dateInput = page.locator("#pomidorqa-slots-date");
+    this.timeInput = page.locator("#pomidorqa-slots-time");
+    this.addSubmitButton = page.getByRole("button", {
+      name: "Добавить слот",
+    });
+    this.firstSlotCard = page.locator("[data-booking-id]").first();
+  }
+
+  async goto() {
+    await this.page.goto(ROUTES.slots);
+  }
+
+  async addSlot(dateStr: string, timeStr: string = "12:00") {
+    await this.dateInput.fill(dateStr);
+    await this.timeInput.fill(timeStr);
+    await this.addSubmitButton.click();
+  }
+}
